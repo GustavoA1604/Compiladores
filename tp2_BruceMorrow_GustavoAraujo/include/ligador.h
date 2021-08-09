12f3e491cd2347528d0c3ba9baf10f106fa33826
GustavoA1604 <gustavogefa@hotmail.com>
2021-08-09 19:17:41 -0300

    Add linkers main.cpp

--- tp2_BruceMorrow_GustavoAraujo/include/ligador.h
+++ tp2_BruceMorrow_GustavoAraujo/include/ligador.h
@@ -1,6 +1,17 @@
 #ifndef LIGADOR_H
 #define LIGADOR_H
 
-// TODO: structs, classes, e outras definições.
+#include <string>
+#include <unordered_map>
+
+namespace lgd {
+
+void processSymbolTable(std::string line, std::unordered_map<std::string, int>& symbolTable, int& initialPosition, int& currentPosition);
+
+void processMetaparameters(std::string line, int& currentPosition);
+
+std::string processCommands(std::string line, std::unordered_map<std::string, int>& symbolTable, int& PCPosition);
+
+}
 
 #endif
--- tp2_BruceMorrow_GustavoAraujo/src/ligador/ligador.cpp
+++ tp2_BruceMorrow_GustavoAraujo/src/ligador/ligador.cpp
@@ -1,3 +1,25 @@
 #include "ligador.h"
 
-// TODO: implementação do editor de ligação
+namespace lgd {
+
+void processSymbolTable(std::string line, 
+                        std::unordered_map<std::string, int>& symbolTable,  
+                        int& initialPosition,
+                        int& currentPosition) {
+  
+
+}
+
+
+void processMetaparameters(std::string line, int& currentPosition) {
+
+}
+
+std::string processCommands(std::string line, 
+                            std::unordered_map<std::string, int>& symbolTable, 
+                            int& PCPosition) {
+  
+  return "";
+}
+
+}
